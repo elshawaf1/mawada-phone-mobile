e58d787efe8af5b088d13f4a7cbb3fc13e81af9a
Ahmed Elshawaf <elshawaf04@gmail.com>
2026-06-27 05:33:52 +0300

    fix: add local Maven repo for Paymob SDK and resolve manifest merger conflict
    - Add withProjectBuildGradle to inject local Maven repo path for Paymob-SDK AAR
    - Add withAndroidManifest to add tools:replace for enableOnBackInvokedCallback

--- plugins/withPaymob.js
+++ plugins/withPaymob.js
@@ -1,6 +1,23 @@
-const { withAppBuildGradle } = require('expo/config-plugins')
+const { withAppBuildGradle, withProjectBuildGradle, withAndroidManifest } = require('expo/config-plugins')
 
 module.exports = function withPaymob(config) {
+  // 1. Add local Maven repo for Paymob SDK AAR to project-level build.gradle
+  config = withProjectBuildGradle(config, (config) => {
+    if (!config.modResults.contents.includes('paymob-reactnative')) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /allprojects\s*\{/,
+        `allprojects {
+    repositories {
+        maven {
+            url = rootProject.projectDir.toURI().resolve("../node_modules/paymob-reactnative/android/libs")
+        }
+    }`
+      )
+    }
+    return config
+  })
+
+  // 2. Enable dataBinding in app-level build.gradle
   config = withAppBuildGradle(config, (config) => {
     if (!config.modResults.contents.includes('dataBinding')) {
       config.modResults.contents = config.modResults.contents.replace(
@@ -10,5 +27,22 @@ module.exports = function withPaymob(config) {
     }
     return config
   })
+
+  // 3. Fix manifest merger conflict: Paymob SDK sets enableOnBackInvokedCallback=true
+  config = withAndroidManifest(config, (config) => {
+    const manifest = config.modResults.manifest
+    if (manifest.$) {
+      manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools'
+    }
+    const application = manifest.application?.[0]
+    if (application?.$) {
+      if (!application.$['tools:replace']) {
+        application.$['tools:replace'] = 'android:enableOnBackInvokedCallback'
+      }
+      application.$['android:enableOnBackInvokedCallback'] = 'false'
+    }
+    return config
+  })
+
   return config
 }
